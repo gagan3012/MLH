c789c01f1431583cb9e12bc6a897b484c4906714
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-16 14:23:08 -0700

    Update app_final.py

--- src/app_final.py
+++ src/app_final.py
@@ -16,3 +16,10 @@ st.sidebar.header("""
 """)
 st.sidebar.markdown(
     "This app allows users to input text of their choice using the tools provided, and ask questions with the answer "
+    "being extracted from the text.")
+st.sidebar.markdown(
+    "_When running the app the first time, it may take some time to initialise due to the requirements needing to be "
+    "downloaded._")
+tool = st.sidebar.selectbox("Tool", ["Mood Analyzer", "Help Me Write My Essay", "Help Me Summarize A Passage", "Wikipedia Answers"])
+
+
