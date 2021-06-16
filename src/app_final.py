dd4d9b095a9ad949054874f2954c77bb7891d587
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-16 14:23:00 -0700

    Create app_final.py

---
+++ src/app_final.py
@@ -0,0 +1,13 @@
+import googlesearch
+import streamlit as st
+from transformers import pipeline
+import requests
+from bs4 import BeautifulSoup
+
+st.set_page_config(
+    page_title="Machine Learning for Hackathons",
+    layout='wide'
+)
+
+
+
